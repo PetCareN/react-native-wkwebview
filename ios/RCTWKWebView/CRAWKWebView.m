526e9eb02a1352f91ad2054ee3d88035359ccdc6
Mark Corner <mcorner@gmail.com>
2019-02-27 15:38:43 -0500

    merge into the upstream version

--- ios/RCTWKWebView/CRAWKWebView.m
+++ ios/RCTWKWebView/CRAWKWebView.m
@@ -563,24 +563,6 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithCoder:(NSCoder *)aDecoder)
   }
 }
 
-- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler {
-  // We sync the wkwebview's cookie store back into the NSHTTPCookieStorage.
-  // If you set sendCookies: true then these saved cookies willbe sent as well.
-  if (@available(iOS 11.0, *)) {
-    WKHTTPCookieStore *cookieStore = webView.configuration.websiteDataStore.httpCookieStore;
-    [cookieStore getAllCookies:^(NSArray* cookies) {
-      if (cookies.count > 0) {
-        for (NSHTTPCookie *cookie in cookies) {
-          NSHTTPCookieStorage *cookieStorage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
-            [cookieStorage setCookie: cookie];
-          }
-      }
-    }];
-  }
-
-  decisionHandler(WKNavigationResponsePolicyAllow);
-}
-
 - (void)webView:(__unused WKWebView *)webView didFailProvisionalNavigation:(__unused WKNavigation *)navigation withError:(NSError *)error
 {
   if (_onLoadingError) {
@@ -703,6 +685,20 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithCoder:(NSCoder *)aDecoder)
 }
 
 - (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler {
+  // We sync the wkwebview's cookie store back into the NSHTTPCookieStorage.
+  // If you set sendCookies: true then these saved cookies willbe sent as well.
+  if (@available(iOS 11.0, *)) {
+    WKHTTPCookieStore *cookieStore = webView.configuration.websiteDataStore.httpCookieStore;
+    [cookieStore getAllCookies:^(NSArray* cookies) {
+      if (cookies.count > 0) {
+        for (NSHTTPCookie *cookie in cookies) {
+          NSHTTPCookieStorage *cookieStorage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
+            [cookieStorage setCookie: cookie];
+          }
+      }
+    }];
+  }
+
   if (_onNavigationResponse) {
     NSDictionary *headers = @{};
     NSInteger statusCode = 200;
@@ -722,5 +718,4 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithCoder:(NSCoder *)aDecoder)
 
   decisionHandler(WKNavigationResponsePolicyAllow);
 }
-
 @end
