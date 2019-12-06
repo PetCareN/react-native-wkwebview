52a75d8ed3454dc217f19ada978103f000b38f07
Mark Corner <mcorner@gmail.com>
2019-12-05 21:12:21 -0500

    update names, see if this works

--- ios/RCTWKWebView/CRAWKWebView.m
+++ ios/RCTWKWebView/CRAWKWebView.m
@@ -16,8 +16,8 @@
 
 // runtime trick to remove WKWebView keyboard default toolbar
 // see: http://stackoverflow.com/questions/19033292/ios-7-uiwebview-keyboard-issue/19042279#19042279
-@interface _SwizzleHelperWK : NSObject @end
-@implementation _SwizzleHelperWK
+@interface _SwizzleHelperWKB : NSObject @end
+@implementation _SwizzleHelperWKB
 -(id)inputAccessoryView
 {
   return nil;
@@ -194,7 +194,7 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithCoder:(NSCoder *)aDecoder)
 
   if(subview == nil) return;
 
-  NSString* name = [NSString stringWithFormat:@"%@_SwizzleHelperWK", subview.class.superclass];
+  NSString* name = [NSString stringWithFormat:@"%@_SwizzleHelperWKB", subview.class.superclass];
   Class newClass = NSClassFromString(name);
 
   if(newClass == nil)
@@ -202,7 +202,7 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithCoder:(NSCoder *)aDecoder)
     newClass = objc_allocateClassPair(subview.class, [name cStringUsingEncoding:NSASCIIStringEncoding], 0);
     if(!newClass) return;
 
-    Method method = class_getInstanceMethod([_SwizzleHelperWK class], @selector(inputAccessoryView));
+    Method method = class_getInstanceMethod([_SwizzleHelperWKB class], @selector(inputAccessoryView));
     class_addMethod(newClass, @selector(inputAccessoryView), method_getImplementation(method), method_getTypeEncoding(method));
 
     objc_registerClassPair(newClass);
